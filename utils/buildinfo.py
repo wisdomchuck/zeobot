f7f933eeae1680b11676f3a6e44a37286fe7e1ca
wisdomchuck <12144116+wisdomchuck@users.noreply.github.com>
2019-12-28 23:42:22 -0500

    buildinfo update

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,5 +1,5 @@
-BUILD_VERSION = "15.2"
-BUILD_AUTHORS = "ZeroEpoch1969#0051 and based robin#0052"
-BUILD_CODENAME = ":yeepensive:"
-BUILD_DATE = "April 14th, 2019 at 6:48 PM GMT -7:00"
+BUILD_VERSION = "15.2.2"
+BUILD_AUTHORS = "WisdomChuck#7806, modified by ZeroEpoch1969#0051 and originally based by robin#0052"
+BUILD_CODENAME = "nugget"
+BUILD_DATE = "December 28th, 2019 at 11:41 PM GMT -5:00"
 TRANSLATORS = {"based robin#0052":"Spanish", "BibleRule#9942":"Hebrew", "Timanttikuutio#1964":"Finnish", "Ele_#0400":"Spanish", "Naratna#1674":"French"}
